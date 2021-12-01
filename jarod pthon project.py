da6846aa68c2f316ae4848a28bd15e492fe753fd
jarodk123 <90394108+jarodk123@users.noreply.github.com>
2021-12-01 09:02:21 -0500

    0.1
    setting up the colors

--- jarod pthon project.py
+++ jarod pthon project.py
@@ -7,3 +7,22 @@ from pygame.locals import *
 pygame.init()
 
 #create game window 
+window surface = pygame.display. set_mode((500,400),0,32)
+pygame.display.set_caption("hello,world")
+
+#set color values
+black = (0, 0, 0)
+white = (255,255,255)
+red   = (255,0, 0)
+green = (0,255 ,0)
+blue  = (0,0,255)
+
+# setup fonts
+basicfont = pygame.font.sysfont(none,48)
+
+#setup text
+text = basicfont.render("hello,world",true, white,blue)
+text rect = text.get_rect()
+textrect.centerx = windowsurface.get_rect().centerx
+textrect.centery = windowsurface.get_rect().centery
+
